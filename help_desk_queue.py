2d143372abc6fdef9a565c46e4f3ff18f1d6acd6
kevwiley <kevwiley05@gmail.com>
2025-09-16 20:24:24 -0400

    Debugged final output issues

--- help_desk_queue.py
+++ help_desk_queue.py
@@ -3,14 +3,49 @@ from node import Node
 
 # Implement your Queue class here
 class Queue:
-    # Delete the following line and implement your Queue class
-    pass
+    def __init__(self): #initializes class
+        self.front = None
+        self.rear = None
+    
+    def enqueue(self,value):
+        newnode = Node(value) 
+        if not self.front: #if self.front has no value, then the newnode is made front value and the rear value
+            self.front = newnode
+            self.rear = newnode
+        else:
+            self.rear.next = newnode #the rear is made the new node
+            self.rear = newnode #the new node is made the rear
+
+    def dequeue(self):
+        if not self.front: #makes sure that there is anything present in the queue,
+            return None
+        removednode = self.front
+        self.front = self.front.next #removes the front node by replacing it with the next one
+
+        if not self.front: #updates the rear to None if the node removed was the last one
+            self.rear = None
+        return removednode.value
+    
+    def peek(self):
+        if self.front:
+            return self.front.value #returns front value that can have something done with it
+        else:
+            return None
+        
+    def print_queue(self):
+        firstnode = self.front #creates variable for first value in the queue
+        if not firstnode: #makes sure the queue isn't empty
+          print("Queue is empty")
+          return
+        while firstnode:
+            print(f"-{firstnode.value}")
+            firstnode = firstnode.next #sets the firstnode variable to the next value in queue
     
 
 
 def run_help_desk():
     # Create an instance of the Queue class
-    
+    helpdesk = Queue()
 
     while True:
         print("\n--- Help Desk Ticketing System ---")
@@ -24,22 +59,31 @@ def run_help_desk():
         if choice == "1":
             name = input("Enter customer name: ")
             # Add the customer to the queue
-            
-            
+            helpdesk.enqueue(name)
             print(f"{name} added to the queue.")
+
         elif choice == "2":
             # Help the next customer in the queue and return message that they were helped
-            pass # delete this line
+            if helpdesk.front: #makes sure there are customers in the queue
+                firstcustomer = helpdesk.front.value
+                helpdesk.dequeue()
+                print(f"Helped {firstcustomer} and removed from queue")
+            else:
+                print("There are no customers left to help")
+
 
 
         elif choice == "3":
             # Peek at the next customer in the queue and return their name
-            pass # delete this line
-
+            if helpdesk.front == None: #makes sure there are customers in the queue
+                print("There are no more customers in the queue")
+            else:
+                print(f"The next customer in the queue is {helpdesk.peek()}")
 
         elif choice == "4":
             # Print all customers in the queue
             print("\nWaiting customers:")
+            helpdesk.print_queue()
             
 
         elif choice == "5":
--- undo_redo_system.py
+++ undo_redo_system.py
@@ -3,11 +3,41 @@ from node import Node
 
 # Implement your Stack class here
 class Stack:
-    pass # delete this line
+    def __init__(self):
+        self.top = None #initializes class
+
+    def push(self, value): #creates node for new value and puts it at top of stack
+        newnode = Node(value)
+        newnode.next = self.top
+        self.top = newnode
+
+    def pop(self): #removes top value from the stack
+        if not self.top: #makes sure that there is something at the top of the stack
+            return None
+        removednode = self.top
+        self.top = self.top.next #removes node by replacing it with the next
+        return removednode.value
+    
+    def peek(self):
+        if self.top: #makes sure stack has something in it
+            return self.top.value #returns first value
+        else:
+            return None
+    
+    def print_stack(self):
+        firstnode = self.top
+        if not firstnode: #makes sure stack isn't empty
+            print("Stack is empty")
+            return
+        while firstnode:
+            print(f"{firstnode.value}")
+            firstnode = firstnode.next #sets the firstnode variable to the next value in stack
+
 
 def run_undo_redo():
     # Create instances of the Stack class for undo and redo
-    
+    undo_stack = Stack() 
+    redo_stack = Stack()
 
     while True:
         print("\n--- Undo/Redo Manager ---")
@@ -22,28 +52,36 @@ def run_undo_redo():
         if choice == "1":
             action = input("Describe the action (e.g., Insert 'a'): ")
             # Push the action onto the undo stack and clear the redo stack
-
+            undo_stack.push(action)
+            redo_stack = Stack()
 
             print(f"Action performed: {action}")
+
         elif choice == "2":
             # Pop an action from the undo stack and push it onto the redo stack
-            pass # delete this line
-            
+            if undo_stack.top == None: #makes sure that there is an action that can be undone
+                print("No actions to undo")
+            else:
+                redo_stack.push(undo_stack.top.value)#adds value to redo_stack before removing from undo_stack
+                undo_stack.pop()
 
         elif choice == "3":
             # Pop an action from the redo stack and push it onto the undo stack
-            pass # delete this line
-
+            if not redo_stack.top:
+                print("No actions to redo")
+            else:
+                undo_stack.push(redo_stack.top.value) #adds value to undo_stack before removing from redo_stack
+                redo_stack.pop()
 
         elif choice == "4":
             # Print the undo stack
             print("\nUndo Stack:")
+            undo_stack.print_stack()
             
-            
-
         elif choice == "5":
             # Print the redo stack
             print("\nRedo Stack:")
+            redo_stack.print_stack()
             
             
             
